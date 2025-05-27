a640912e921cffa7e019eac99fd4e6b7e1f12655
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2025-05-27 08:57:23 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,14 +4,15 @@ console.log({
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME
+  DB_NAME: process.env.DB_NAME,
+  DB_CHARSET: process.env.DB_CHARSET,
 });
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-    //charset: 'latin1',
+charset: process.env.DB_CHARSET,
 
   connectionLimit: 10
 });
