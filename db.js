1e440c27c23880b91f0169053c690e6e815a4dad
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2025-05-27 08:51:34 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 // db.js
 const mysql = require('mysql2/promise'); // usa mysql2 con promesas
-
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS ? '******' : undefined,
+  DB_NAME: process.env.DB_NAME
+});
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
