db4a75fe18767d1eccbb536e9efc70d39b1ba147
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2025-05-27 09:42:35 +0200

    Update db.js

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  charset: config.DB_CHARSET || 'utf8mb4', 
+  charset: process.DB_CHARSET || 'utf8mb4', 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
