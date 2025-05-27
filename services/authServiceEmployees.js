12f59d54fcf22e68152d55c9fabba940536243f2
Patty <pqtriciacm@gmail.com>
2025-05-27 19:05:13 +0200

    efw

--- services/authServiceEmployees.js
+++ services/authServiceEmployees.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { query } = require('../db'); 
-import connect from '../db';
 
 const authenticateEmployee = async (email, password) => {
   console.log('authenticateEmployee -> email:', email, 'password:', password);
