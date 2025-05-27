de898df280bf61586956caf96885e74d348ba2b6
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2025-05-27 09:52:35 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,22 +1,18 @@
-// db.js
 const mysql = require('mysql2/promise'); // usa mysql2 con promesas
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-  DB_CHARSET: process.DB_CHARSET, 
-});
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  charset: process.DB_CHARSET || 'utf8mb4', 
+const config = {
+  host: 'bbdd.pattydev.com',
+  user: 'ddb254183',
+  password: '03111965.pcM',
+  database: 'ddb254183',
+  charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
+
+console.log('Configuración de conexión a MySQL:', config);
+
+const pool = mysql.createPool(config);
 
 module.exports = pool;
