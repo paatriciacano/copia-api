70c3680e449d8161685b897d0c162ba78dcab7ea
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2025-05-27 13:22:52 +0200

    Update db.js

--- db.js
+++ db.js
@@ -16,6 +16,8 @@ console.log('Configuración de conexión a MySQL:', config);
 const pool = mysql.createPool(config);
 pool.on('connection', async (connection) => {
   await connection.query("SET NAMES 'latin1'");
+  const [rows] = await connection.query("SHOW VARIABLES LIKE 'character_set_client'");
+  console.log('Charset connection:', rows);
 });
 
 module.exports = pool;
