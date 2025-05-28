3624b328f899d5571683458fa1089061bdcda53e
Patty <pqtriciacm@gmail.com>
2025-05-28 18:52:06 +0200

    vdd

--- db.js
+++ db.js
@@ -1,23 +1,22 @@
-const mysql = require('mysql2/promise');
 
-async function test() {
-  try {
-    const connection = await mysql.createConnection({
-      host: 'bbdd.pattydev.com',
-      user: 'ddb254183',
-      password: '03111965.pcM',
-      database: 'ddb254183',
-      charset: 'latin1'
+const mysql = require('mysql2');
 
-    });
+const db = mysql.createConnection({
+  host: 'bbdd.pattydev.com',
+  user: 'ddb254183',
+  password: '03111965.pcM',
+  database: 'ddb254183',
 
-    const [rows] = await connection.query('SELECT NOW() as now');
-    console.log(rows);
 
-    await connection.end();
-  } catch (error) {
-    console.error('Error:', error);
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a MySQL:', err.message);
+    process.exit(1);
+  } else {
+    console.log('Conexión a MySQL establecida');
   }
-}
+});
 
-test();
+module.exports = db;
