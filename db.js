f5e3fd8604786a346a544525cb57bb0095d0dc31
Patty <pqtriciacm@gmail.com>
2025-05-28 19:58:36 +0200

    vsd

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  charset: process.env.DB_CHARSET || 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
