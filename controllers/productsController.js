2acb240398043d3917684b59e9894bfb941e4421
VictorHacks <victor_po98@hotmail.com>
2025-05-30 05:03:40 +0200

    API Fixed

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,7 +4,8 @@ const productsService = require('../services/productsService');
 const getProducts = async (req, res) => {
   try {
     const products = await productsService.getAllProducts();  
-    res.status(200).json(products); 
+    if (!products) return res.status(200).json({ message: 'Sin productos actualmente' });
+    else res.json(products); 
   } catch (err) {
     res.status(500).json({ error: err.message });  
   }
--- db.js
+++ db.js
@@ -1,31 +1,3 @@
-/*const mysql = require('mysql2');
-const dotenv = require("dotenv");
-dotenv.config();
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  charset: process.env.DB_CHARSET || 'utf8mb4',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Error de conexión inicial:', err.message);
-  } else {
-    console.log('Conectado exitosamente al pool');
-    connection.release();
-  }
-});
-
-const promisePool = pool.promise();
-module.exports = promisePool;
-*/
-
 const mysql = require('mysql')
 
 const { database } = require('./keys')
@@ -43,16 +15,5 @@ conexion.getConnection((error, connection) => {
 })
 
 conexion.query = promisify(conexion.query)
-
-/*const mysql = require('mysql2');
-const dotenv = require("dotenv");
-dotenv.config();
-
-const conexion = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME    
-})*/
 
 module.exports = conexion
